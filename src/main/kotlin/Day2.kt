4d89320f71a80b24db641911ff09e457c068f9f0
laurent gautho-lapeyre <github@laurent-gautho-lapeyre.fr>
2021-12-07 22:31:33 +0100

    Day 2 part 2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,53 +1,107 @@
 fun main() {
     println("part 1 :")
-    println(day2part1(readLinesOf("day2.txt")))
+    println(Day2Part1.calculate(readLinesOf("day2.txt")))
+    println("part 2 :")
+    println(Day2Part2.calculate(readLinesOf("day2.txt")))
 }
 
-fun day2part1(lines: List<String>): Int {
-    return lines
-        .map(Order.Companion::parse)
-        .fold(SubmarinePosition()) { currentPosition, order ->
-            when(order.type) {
-                OrderType.forward -> currentPosition.addPosition(order.value)
-                OrderType.down -> currentPosition.increaseDepth(order.value)
-                OrderType.up -> currentPosition.decreaseDepth(order.value)
-            }
-        }.let {
-            it.depth * it.position
-        }
-}
-
-data class SubmarinePosition(
-    val position: Int = 0,
-    val depth: Int = 0
-) {
-    fun addPosition(value: Int): SubmarinePosition {
-        return this.copy(position = position + value)
-    }
-
-    fun increaseDepth(value: Int): SubmarinePosition {
-        return this.copy(depth = depth + value)
-    }
-
-    fun decreaseDepth(value: Int): SubmarinePosition {
-        return this.copy(depth = depth - value)
-    }
-}
-
-data class Order(
-    val type: OrderType,
-    val value: Int
-) {
+class Day2Part1 {
     companion object {
-        fun parse(str: String): Order {
-            val (order, value) = str.split(" ")
-            return Order(OrderType.valueOf(order), value.toInt())
+        fun calculate(lines: List<String>): Int {
+            return lines
+                .map(Order.Companion::parse)
+                .fold(SubmarinePosition()) { currentPosition, order ->
+                    when (order.type) {
+                        OrderType.forward -> currentPosition.addPosition(order.value)
+                        OrderType.down -> currentPosition.increaseDepth(order.value)
+                        OrderType.up -> currentPosition.decreaseDepth(order.value)
+                    }
+                }.let {
+                    it.depth * it.position
+                }
         }
+    }
+
+    data class SubmarinePosition(
+        val position: Int = 0,
+        val depth: Int = 0
+    ) {
+        fun addPosition(value: Int): SubmarinePosition {
+            return this.copy(position = position + value)
+        }
+
+        fun increaseDepth(value: Int): SubmarinePosition {
+            return this.copy(depth = depth + value)
+        }
+
+        fun decreaseDepth(value: Int): SubmarinePosition {
+            return this.copy(depth = depth - value)
+        }
+    }
+
+    data class Order(
+        val type: OrderType,
+        val value: Int
+    ) {
+        companion object {
+            fun parse(str: String): Order {
+                val (order, value) = str.split(" ")
+                return Order(OrderType.valueOf(order), value.toInt())
+            }
+        }
+    }
+
+    enum class OrderType {
+        forward,
+        down,
+        up,
     }
 }
 
-enum class OrderType {
-    forward,
-    down,
-    up,
+class Day2Part2 {
+    companion object {
+        fun calculate(lines: List<String>): Int {
+            return lines
+                .map(Order.Companion::parse)
+                .fold(SubmarinePosition()) { currentPosition, order ->
+                    currentPosition.execute(order)
+                }.let {
+                    it.depth * it.position
+                }
+        }
+    }
+
+    data class SubmarinePosition(
+        val position: Int = 0,
+        val depth: Int = 0,
+        val aim: Int = 0
+    ) {
+        fun execute(order: Order): SubmarinePosition {
+            val x = order.value
+
+            return when (order.type) {
+                OrderType.forward -> copy(position = position + x, depth = depth + aim * x)
+                OrderType.down -> copy(aim = aim + x)
+                OrderType.up -> copy(aim = aim - x)
+            }
+        }
+    }
+
+    data class Order(
+        val type: OrderType,
+        val value: Int
+    ) {
+        companion object {
+            fun parse(str: String): Order {
+                val (order, value) = str.split(" ")
+                return Order(OrderType.valueOf(order), value.toInt())
+            }
+        }
+    }
+
+    enum class OrderType {
+        forward,
+        down,
+        up,
+    }
 }
--- src/test/kotlin/Day2.kt
+++ src/test/kotlin/Day2.kt
@@ -6,6 +6,13 @@ class Day2 {
     internal fun part1() {
         val lines = readLinesOf("day2_example.txt")
 
-        day2part1(lines) shouldBe 150
+        Day2Part1.calculate(lines) shouldBe 150
+    }
+
+    @Test
+    internal fun part2() {
+        val lines = readLinesOf("day2_example.txt")
+
+        Day2Part2.calculate(lines) shouldBe 900
     }
 }
