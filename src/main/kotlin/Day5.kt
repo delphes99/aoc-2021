70db73e902cc4a88fd4116c43a1b282ba5043bc2
laurent gautho-lapeyre <github@laurent-gautho-lapeyre.fr>
2021-12-11 15:35:31 +0100

    Day 5 part 2

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,9 +1,13 @@
 import java.lang.Integer.min
 import java.lang.Integer.max
+import kotlin.math.sign
 
 fun main() {
     println("part 1 :")
     println(Day5Part1.calculate(readLinesOf("day5.txt")))
+
+    println("part 2 :")
+    println(Day5Part2.calculate(readLinesOf("day5.txt")))
 }
 
 class Day5Part1 {
@@ -31,6 +35,48 @@ class Day5Part1 {
     }
 
     data class Point(
+        val x: Int,
+        val y: Int,
+    )
+}
+
+class Day5Part2 {
+    companion object {
+        fun toPoints(lines: List<String>): List<Point> {
+            return lines.flatMap { line ->
+                val (origin, destination) = line.split(" -> ")
+                    .map { it.split(",") }
+                    .map { it.map(String::toInt) }
+                    .map { Point(it[0], it[1]) }
+
+                val vectorFromOrigin = Vector((destination.x - origin.x).sign, (destination.y - origin.y).sign)
+
+                val points = mutableListOf(origin)
+                var current = origin
+                while (current != destination) {
+                    current = current.move(vectorFromOrigin)
+                    points.add(current)
+                }
+
+                points
+            }
+        }
+
+        fun calculate(lines: List<String>): Int {
+            return toPoints(lines).groupBy { it }.filterValues { it.size > 1 }.keys.size
+        }
+    }
+
+    data class Point(
+        val x: Int,
+        val y: Int,
+    ) {
+        fun move(vector: Vector): Point {
+            return Point(x + vector.x, y + vector.y)
+        }
+    }
+
+    data class Vector(
         val x: Int,
         val y: Int,
     )
--- src/test/kotlin/Day5.kt
+++ src/test/kotlin/Day5.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 
 class Day5 {
     @Test
-    internal fun `convert input to points`() {
+    internal fun `convert input to points (part 1)`() {
         Day5Part1.toPoints(
             listOf(
                 "1,1 -> 1,3",
@@ -24,5 +24,32 @@ class Day5 {
     @Test
     internal fun part1() {
         Day5Part1.calculate(readLinesOf("day5_example.txt")) shouldBe 5
+    }
+
+    @Test
+    internal fun `convert input to points (part 2)`() {
+        Day5Part2.toPoints(
+            listOf(
+                "1,1 -> 1,3",
+                "9,7 -> 7,7",
+                "1,0 -> 4,3",
+            )
+        ).shouldContainExactlyInAnyOrder(
+            Day5Part2.Point(1, 1),
+            Day5Part2.Point(1, 2),
+            Day5Part2.Point(1, 3),
+            Day5Part2.Point(9, 7),
+            Day5Part2.Point(8, 7),
+            Day5Part2.Point(7, 7),
+            Day5Part2.Point(1, 0),
+            Day5Part2.Point(2, 1),
+            Day5Part2.Point(3, 2),
+            Day5Part2.Point(4, 3),
+        )
+    }
+
+    @Test
+    internal fun part2() {
+        Day5Part2.calculate(readLinesOf("day5_example.txt")) shouldBe 12
     }
 }
