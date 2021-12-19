32f4d2a696e7ae21ffd4a0a08a1582b13c9c990e
laurent gautho-lapeyre <github@laurent-gautho-lapeyre.fr>
2021-12-19 17:07:12 +0100

    Day 6 part 2

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,11 +1,14 @@
 fun main() {
     println("part 1 :")
     println(Day6Part1.calculationPopulationSizeAt(readLinesOf("day6.txt").first(), 80))
+
+    println("part 2 :")
+    println(Day6Part2.calculationPopulationSizeAt(readLinesOf("day6.txt").first(), 256))
 }
 
 class Day6Part1 {
     companion object {
-        fun calculationPopulationSizeAt(initialPopulationStr: String, generation: Int) : Int {
+        fun calculationPopulationSizeAt(initialPopulationStr: String, generation: Int): Int {
             val initialPopulation = initialPopulationStr.split(",").map(String::toInt)
             return calculateGenerations(initialPopulation, generation).last().size
         }
@@ -27,5 +30,48 @@ class Day6Part1 {
     }
 }
 
+class Day6Part2 {
+    companion object {
+        fun calculationPopulationSizeAt(initialPopulationStr: String, generation: Int): Long {
+            val initialPopulation = initialPopulationStr.split(",").map(String::toInt).groupBy { it }
+                .mapValues { (_, population) -> population.size.toLong() }
+            return calculateGenerations(initialPopulation, generation).last().values.sum()
+        }
+
+        fun calculateGenerations(
+            initialPopulationStr: Population2,
+            numberOfGeneration: Int
+        ): List<Population2> {
+            val generations = mutableListOf(initialPopulationStr)
+            for (i in 1..numberOfGeneration) {
+                generations.add(generation(generations.last()))
+            }
+            return generations.drop(1)
+        }
+
+        fun generation(population: Population2): Population2 {
+            val fishGivingBirth = population[0] ?: 0
+
+            val oldFish = population.map { (age, numberOfFish) ->
+                if (age == 0) {
+                    6
+                } else {
+                    age - 1
+                } to numberOfFish
+            }
+
+
+            return if (fishGivingBirth > 0) {
+                oldFish.plus(8 to fishGivingBirth)
+            } else {
+                oldFish
+            }
+                .groupBy { (age, _) -> age }
+                .mapValues { (_, populations) -> populations.map(Pair<Int, Long>::second).sum() }
+        }
+    }
+}
+
 typealias FishAge = Int
 typealias Population = List<FishAge>
+typealias Population2 = Map<FishAge, Long>
--- src/test/kotlin/Day6.kt
+++ src/test/kotlin/Day6.kt
@@ -27,4 +27,91 @@ internal class Day6 {
     internal fun `population with birth`() {
         Day6Part1.generation(listOf(2, 3, 2, 0, 1)).shouldContainExactly(1, 2, 1, 6, 0, 8)
     }
+
+    @Test
+    internal fun part2() {
+        Day6Part2.calculationPopulationSizeAt("3,4,3,1,2", 256) shouldBe 26984457539
+    }
+
+    @Test
+    internal fun `population with birth (pt2)`() {
+        Day6Part2.generation(
+            mapOf(
+                2 to 2L,
+                3 to 1L,
+                0 to 1L,
+                1 to 1L
+            )
+        ).entries.map { it.toPair() }.shouldContainExactly(
+            1 to 2L,
+            2 to 1L,
+            6 to 1L,
+            0 to 1L,
+            8 to 1L
+        )
+    }
+
+    @Test
+    internal fun `population with merge (pt2)`() {
+        Day6Part2.generation(
+            mapOf(
+                2 to 2L,
+                3 to 1L,
+                0 to 1L,
+                1 to 1L,
+                7 to 3L
+            )
+        ).entries.map { it.toPair() }.shouldContainExactly(
+            1 to 2L,
+            2 to 1L,
+            6 to 4L,
+            0 to 1L,
+            8 to 1L
+        )
+    }
+
+    @Test
+    internal fun `calculate generations bis`() {
+        val lines = readLinesOf("day6_example_result.txt")
+
+        Day6Part2.calculateGenerations(
+            mapOf(
+                3 to 2L,
+                4 to 1L,
+                1 to 1L,
+                2 to 1L
+            ), 18
+        ).forEachIndexed { index, generation ->
+            println(index)
+            generation.shouldBe(
+                lines[index]
+                    .split(",")
+                    .map(String::toInt)
+                    .groupBy { it }
+                    .mapValues { (_, fishes) -> fishes.size.toLong() }
+            )
+        }
+    }
+
+    @Test
+    internal fun `calculate generations (pt2)`() {
+        Day6Part2.calculateGenerations(
+            mapOf(
+                3 to 2L,
+                4 to 1L,
+                1 to 1L,
+                2 to 1L
+            ), 18
+        ).last() shouldBe mapOf(
+            0 to 3L,
+            1 to 5L,
+            2 to 3L,
+            3 to 2L,
+            4 to 2L,
+            5 to 1L,
+            6 to 5L,
+            7 to 1L,
+            8 to 4L
+        )
+    }
 }
