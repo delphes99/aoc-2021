84ed4ba59a8930c50f61c6ae4a0024f56d8867b6
laurent gautho-lapeyre <github@laurent-gautho-lapeyre.fr>
2022-01-07 15:42:30 +0100

    Day 7 part 2

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -3,6 +3,9 @@ import kotlin.math.absoluteValue
 fun main() {
     println("part 1 :")
     println(Day7Part1.calculate(readLinesOf("day7.txt").first()))
+
+    println("part 2 :")
+    println(Day7Part2.calculate(readLinesOf("day7.txt").first()))
 }
 
 class Day7Part1 {
@@ -25,6 +28,30 @@ class Day7Part1 {
 
         fun cost(target: Int, horizontalPositions: List<Int>): Int {
             return horizontalPositions.sumOf { (it - target).absoluteValue }
+        }
+    }
+}
+
+class Day7Part2 {
+    companion object {
+        fun calculate(horizontalPositionsStr: String): Int {
+            val horizontalPositions = horizontalPositionsStr.split(",").map { it.toInt() }
+
+            val targetPosition = calculateHorizontalPosition(horizontalPositions)
+            return cost(targetPosition, horizontalPositions)
+        }
+
+        fun calculateHorizontalPosition(horizontalPositions: List<Int>): Int {
+            val allValues = (0..(horizontalPositions.maxOrNull()?:0))
+
+            val costsByPosition = allValues.associateWith { cost(it, horizontalPositions) }
+            val minCost = costsByPosition.values.minOrNull()
+
+            return costsByPosition.filterValues { it == minCost }.keys.first()
+        }
+
+        fun cost(target: Int, horizontalPositions: List<Int>): Int {
+            return horizontalPositions.map { (it - target).absoluteValue }.sumOf { it * (it + 1) / 2 }
         }
     }
 }
--- src/test/kotlin/Day7.kt
+++ src/test/kotlin/Day7.kt
@@ -8,7 +8,22 @@ internal class Day7 {
     }
 
     @Test
-    internal fun `horizontal position`() {
+    internal fun `horizontal position part1`() {
         Day7Part1.calculateHorizontalPosition(listOf(16, 1, 2, 0, 4, 2, 7, 1, 2, 14)) shouldBe 2
+    }
+
+    @Test
+    internal fun part2() {
+        Day7Part2.calculate("16,1,2,0,4,2,7,1,2,14") shouldBe 168
+    }
+
+    @Test
+    internal fun `cost part2`() {
+        Day7Part2.cost(5, listOf(16, 1, 2, 0, 4, 2, 7, 1, 2, 14)) shouldBe 168
+    }
+
+    @Test
+    internal fun `horizontal position part2`() {
+        Day7Part2.calculateHorizontalPosition(listOf(16, 1, 2, 0, 4, 2, 7, 1, 2, 14)) shouldBe 5
     }
 }
